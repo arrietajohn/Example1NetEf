240d7e1e10e2e89bd7fb2a7a04fe3956184c43da
JOHN CARLOS ARRIETA ARRIETA <johncarlosarrietaarrieta@gmail.com>
2024-05-12 01:10:09 -0500

    El Proyecto de la capa de Dominio

---
+++ CleanArchitecture.Domain/Actor.cs
@@ -0,0 +1,19 @@
+﻿
+using MyProjectDotNet.Domain.Common;
+
+namespace MyProjectDotNet.Domain
+{
+    public class Actor : BaseDomainModel
+    {
+        public Actor() { 
+            Videos = new HashSet<Video>();
+        }
+        
+        public string? Nombre { get; set; }
+
+        public string? Apellido { get; set; }
+
+        public virtual ICollection<Video> Videos { get; set; }
+
+    }
+}
---
+++ CleanArchitecture.Domain/Streamer.cs
@@ -0,0 +1,14 @@
+﻿using MyProjectDotNet.Domain.Common;
+
+namespace MyProjectDotNet.Domain
+{
+    public class Streamer : BaseDomainModel
+    {
+        public string? Nombre { get; set; }
+
+        public string? Url { get; set; }
+
+        public ICollection<Video>? Videos { get; set; }
+
+    }
+}
